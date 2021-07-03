ac5325573a7869e4782fb695174f3e50987e94d6
vitorwlima <vitorwlima13@gmail.com>
2021-07-03 17:27:25 -0300

    added home page and initial components

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, InputWrapper } from './styles'
+
+import { Input, Select } from '../../components'
 
 const Home = () => {
+  const [questionsNumber, setQuestionsNumber] = useState(10)
+  const [selectedCategory, setSelectedCategory] = useState('any')
+  const [selectedDifficulty, setSelectedDifficulty] = useState('easy')
+
+  const categoryOptions = [
+    {
+      value: 'any',
+      text: 'Any Category',
+    },
+    {
+      value: '9',
+      text: 'General Knowledge',
+    },
+  ]
+
+  const difficultyOptions = [
+    {
+      value: 'easy',
+      text: 'Easy',
+    },
+    {
+      value: 'medium',
+      text: 'Medium',
+    },
+    {
+      value: 'hard',
+      text: 'Hard',
+    },
+  ]
+
   return (
-    <div>
-      home
-    </div>
+    <Container>
+      <h1>Set up your quiz!</h1>
+      <InputWrapper>
+        <Input
+          type='number'
+          nameId='questionsNumber'
+          label='Number of questions:'
+          min={0}
+          value={questionsNumber}
+          onChange={e => setQuestionsNumber(+e.target.value)}
+        />
+        <Select
+          nameId='category'
+          options={categoryOptions}
+          value={selectedCategory}
+          label='Select category:'
+          onChange={e => setSelectedCategory(e.target.value)}
+        />
+        <Select
+          nameId='difficulty'
+          options={difficultyOptions}
+          value={selectedDifficulty}
+          label='Select a difficulty:'
+          onChange={e => setSelectedDifficulty(e.target.value)}
+        />
+      </InputWrapper>
+    </Container>
   )
 }
 
---
+++ src/views/Home/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  h1 {
+    text-align: center;
+    margin-bottom: 24px;
+  }
+
+  margin: 20px 40px;
+`
+
+export const InputWrapper = styled.div``
